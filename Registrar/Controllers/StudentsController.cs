7f70a7e5909c0e6ac5c0344b1f6dca850fb2b2e5
Callie Stump <callie@stu.mp>
2021-01-04 16:59:11 -0500

    adds elements for students
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>

---
+++ Registrar/Controllers/StudentsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Registrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Registrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly RegistrarContext _db;
+    public StudentsController(RegistrarContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Students.ToList());
+    }
+    // public ActionResult Create()
+    // {
+      
+    // }
+    public ActionResult Details(int id)
+    {
+      var thisStudent = _db.Students
+          .Include(student => student.Courses)
+          .ThenInclude(join => join.Courses)
+          .FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
+  }
+}
---
+++ Registrar/Models/Students.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+namespace Registrar.Models
+{
+  public class Student
+  {
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+
+  }
+}
